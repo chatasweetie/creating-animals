77213b5e38ac289587b5416354dbd1752e7a8a86
Jessica <jessica.dene.earley@gmail.com>
2016-07-21 11:42:43 -0700

    added emoji icons and a generic animal class

--- create.py
+++ create.py
@@ -1,10 +1,17 @@
 """Creates Cats & Dogs"""
 import random
 import names
+import emoji
+
+possible_animals = [':mouse:', ':hamster:', ':rabbit:', ':koala:', 
+                    ':frog:', ':monkey:', ':panda_face:', ':fish:']
 
 
 class AbstractAnimal(object):
     """The idea of an animal"""
+
+    heart = ':heartpulse:'
+    sad = ':broken_heart:'
 
     def __init__(self, name):
         self.name = name
@@ -12,12 +19,15 @@ class AbstractAnimal(object):
         self.obedience_level = random.randrange(0, 10)
         self.hunger = random.randrange(0, 10)
         self.cranky = random.randrange(0, 10)
-        self.mortal_enemy = []
-        self.best_friends = []
+        self.friendly = random.randrange(0, 10)
+        self.mortal_enemy = set()
+        self.best_friends = set()
+        self.frenemies = set()
         self.spay_or_neuter = False
 
     def __repr__(self):
         """Provides useful represenation when printed"""
+        print emoji.emojize(self.smile, use_aliases=True)
 
         return """<{} name: {} >""".format(self.species, self.name)
 
@@ -25,13 +35,15 @@ class AbstractAnimal(object):
         """What happens when you add two animals"""
 
         if self.cranky > 7:
-            self.mortal_enemy.append(other)
+            self.mortal_enemy.add(other)
 
-            return"{} is {} mortal enemy".format(other.name, self.name)
+            return"{} is {} mortal enemy".format(other.name, (self.name + '\'s'))
 
-        self.best_friends.append(other)
+        self.best_friends.add(other)
 
-        return "{} is {} best friend".format(other.name, self.name)
+        print emoji.emojize(self.smile, use_aliases=True),  emoji.emojize(other.smile, use_aliases=True)
+
+        return "{} is {} best friend".format(other.name, (self.name + '\'s'))
 
     def __sub__(self, other):
         """What happens when you subtract two animals"""
@@ -41,11 +53,12 @@ class AbstractAnimal(object):
         """What happens when you multiply two animals"""
 
         if self.species != other.species:
-            return "nope"
+            return "You cannot breed between species"
 
         if self.spay_or_neuter is True or other.spay_or_neuter is True:
-            return "cannot multiply {} due to animal being spayed/neutered".format((self.species + 's'))
+            return "cannot breed {} due to animal being spayed/neutered".format((self.species + 's'))
 
+        print emoji.emojize(self.heart, use_aliases=True) * 2
         litter_size = random.randrange(0, 5)
         litter = []
         for baby in range(litter_size):
@@ -70,6 +83,10 @@ class Cat(AbstractAnimal):
 
     greet = "Meow"
     species = "cat"
+    # emojis
+    smile = ':cat:'
+    heart = ':heart_eyes_cat:'
+    sad = ':crying_cat_face:'
 
     def __init__(self, name):
         super(Cat, self).__init__(name)
@@ -81,7 +98,24 @@ class Dog(AbstractAnimal):
 
     greet = "Woof"
     species = "dog"
+    # emojis
+    smile = ':dog:'
+
 
     def __init__(self, name):
         self.loyality_level = random.randrange(0, 10)
         super(Dog, self).__init__(name)
+
+
+class Animal(AbstractAnimal):
+    """An animal"""
+
+    greet = "hey"
+    species = ""
+    # emojis
+
+
+    def __init__(self, name):
+        super(Animal, self).__init__(name)
+        self.smile = random.choice(possible_animals)
+
